b57a8172fafeb94e73393d876e554e0211692209
iarmolla <ivan.armolla@gmail.com>
2023-01-24 23:10:37 -0300

    changing api key

--- src/services/emailValidation.js
+++ src/services/emailValidation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const validate = async (email) => {
   const response = await axios.get(
-    `https://emailvalidation.abstractapi.com/v1/?api_key=7e387e01d6e74150904498952e707aae&email=${email}`
+    `https://emailvalidation.abstractapi.com/v1/?api_key=YOUR_API_KEY=${email}`
   );
   return response?.data?.deliverability ;
 };
